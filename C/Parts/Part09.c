a6e6ab9de0a65dbf853865d86e6e387e2ef53a17
AntiJ0 <firefly17569@gmail.com>
2024-05-02 15:51:49 +0900

    무기 교체 프로그램

--- C/Parts/Part09.c
+++ C/Parts/Part09.c
@@ -3,7 +3,9 @@
 
 void Part09()
 {
-    Structure();
+    //Structure();
+    //Union();
+    Enumeration();
 }
 
 void Structure()
@@ -46,4 +48,90 @@ void Structure()
 void PrintPlayerInfo(Player player)
 {
     printf("플레이어 %d 정보 [ Level : %d, HP : %d, ATK : %d ]\n", player.id, player.level, player.hp, player.atk);
+}
+
+void Union()
+{
+    printf("공용체 크기 : %zd\n", sizeof(Data));
+
+    Data data = { 0 };
+    data.intNum = 123456789;
+
+    printf("\n===== Int 할당 결과 =====\n");
+    printf("Int : %d\n", data.intNum);
+    printf("float : %f\n", data.fltNum);
+    printf("double : %lf\n", data.dbNum);
+
+    data.fltNum = 3.14f;
+
+    printf("\n===== Float 할당 결과 =====\n");
+    printf("Int : %d\n", data.intNum);
+    printf("float : %f\n", data.fltNum);
+    printf("double : %lf\n", data.dbNum);
+
+    data.dbNum = 3.141592;
+
+    printf("\n===== Double 할당 결과 =====\n");
+    printf("Int : %d\n", data.intNum);
+    printf("float : %f\n", data.fltNum);
+    printf("double : %lf\n", data.dbNum);
+
+}
+
+void Enumeration()
+{
+    Status status = STATUS_NONE;
+    Motion motion = MOTION_NONE;
+
+    printf("===== 열거형 상수 =====\n");
+    printf("Gold : %d\n", ITEM_GOLD);
+    printf("Weapon : %d\n", ITEM_WEAPON);
+    printf("Armor : %d\n", ITEM_ARMOR);
+
+    ItemType itemType = ITEM_WEAPON;
+
+    printf("\n===== 열거형 상수 =====\n");
+    printf("ItemType is Gold : %s\n", itemType == ITEM_GOLD ? "True" : "False");
+    printf("ItemType is Weapon : %s\n", itemType == ITEM_WEAPON ? "True" : "False");
+    printf("ItemType is Armor : %s\n", itemType == ITEM_ARMOR ? "True" : "False");
+
+    system("pause");
+
+    if (itemType == ITEM_WEAPON)
+    {
+        WeaponType weaponType = WEAPON_NONE;
+
+        do
+        {
+            system("cls");
+            printf("===== 무기 교체 프로그램 ===== \n\n");
+            printf("1. 검 2. 활 3. 총 4. 나가기 \n\n메뉴 입력 : ");
+            scanf("%d", &weaponType);
+            --weaponType;
+            printf("\n");
+
+            switch (weaponType)
+            {
+            case WEAPON_SWORD:
+                printf("현재 무기 : 검");
+                break;
+            case WEAPON_BOW:
+                printf("현재 무기 : 활");
+                break;
+            case WEAPON_GUN:
+                printf("현재 무기 : 총");
+                break;
+            case WEAPON_MAX_VALUE:
+                printf("프로그램을 종료합니다.");
+                break;
+            default:
+                printf("잘못 입력하셨습니다. 다시 입력해주세요.");
+                break;
+            }
+
+            printf("\n\n");
+            system("pause");
+
+        } while (weaponType != WEAPON_MAX_VALUE);
+    }
 }
--- C/Parts/Part09.h
+++ C/Parts/Part09.h
@@ -38,3 +38,58 @@ typedef struct Player
 
 void Structure();
 void PrintPlayerInfo(Player player);
+
+typedef union Data
+{
+    int intNum;
+    float fltNum;
+    double dbNum;
+} Data;
+
+void Union();
+
+typedef enum Color
+{
+    COLOR_NONE = -1,
+    COLOR_RED,
+    COLOR_GREEN = 10,
+    COLOR_BLUE,
+    COLOR_MAX_VALUE
+} Color;
+
+typedef enum Status
+{
+    STATUS_NONE,
+    STATUS_ALIVE,
+    STATUS_DEAD,
+    STATUS_MAX_VALUE
+} Status;
+
+typedef enum Motion
+{
+    MOTION_NONE,
+    MOTION_IDLE,
+    MOTION_WALKING,
+    MOTION_RUNNING,
+    MOTION_MAX_VALUE
+} Motion;
+
+typedef enum ItemType
+{
+    ITEM_NONE = -1,
+    ITEM_GOLD,
+    ITEM_WEAPON,
+    ITEM_ARMOR,
+    ITEM_MAX_VALUE
+} ItemType;
+
+typedef enum WeaponType
+{
+    WEAPON_NONE = -1,
+    WEAPON_SWORD,
+    WEAPON_BOW,
+    WEAPON_GUN,
+    WEAPON_MAX_VALUE
+} WeaponType;
+
+void Enumeration();
