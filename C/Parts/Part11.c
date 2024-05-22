5dae478046cc543d9364d1336f67e5ec2ecdf7de
AntiJ0 <firefly17569@gmail.com>
2024-05-22 14:40:05 +0900

    Part11 완료

--- C/Parts/Part11.c
+++ C/Parts/Part11.c
@@ -3,7 +3,8 @@
 
 void Part11()
 {
-	FunctionPointer();
+	//FunctionPointer();
+	FunctionPointerExample();
 }
 
 void FunctionPointer()
@@ -51,8 +52,72 @@ void FuncB()
 	printf("FuncB 호출!\n");
 }
 
-void(*FuncC(int funcType))()
+//void(*FuncC(int funcType))()
+//{
+//	printf("FuncC => ");
+//	return funcType <= 0 ? &FuncA : &FuncB;
+//}
+
+FuncPtr_void_void FuncC(int funcType)
 {
 	printf("FuncC => ");
 	return funcType <= 0 ? &FuncA : &FuncB;
+}
+
+/*====================================================*/
+
+void FunctionPointerExample()
+{
+	srand((unsigned)time(NULL));
+
+	int arr[10] = { 0 };
+	const int len = sizeof(arr) / sizeof(arr[0]);
+
+	for (int i = 0; i < len; ++i)
+		arr[i] = rand() % 100 + 1;
+
+	printf("===== 정렬 전 ===== \n");
+	for (int i = 0; i < len; ++i)
+		printf("%d ", arr[i]);
+
+	printf("\n\n===== 오름차순 정렬 후 ===== \n");
+	SortArray(arr, len, &CompareByAscending);
+	for (int i = 0; i < len; ++i)
+		printf("%d ", arr[i]);
+
+	printf("\n\n===== 내림차순 정렬 후 ===== \n");
+	SortArray(arr, len, &CompareByDescending);
+	for (int i = 0; i < len; ++i)
+		printf("%d ", arr[i]);
+}
+
+void SortArray(int* arr, int len, bool(*compareFunc)(int, int))
+{
+	for (int i = 0; i < len; ++i)
+	{
+		int compareIndex = i;
+
+		for (int j = i + 1; j < len; ++j)
+		{
+			if (compareFunc(arr[compareIndex], arr[j]) == true)
+				compareIndex = j;
+		}
+
+		if (compareIndex != i)
+		{
+			int temp = arr[i];
+			arr[i] = arr[compareIndex];
+			arr[compareIndex] = temp;
+		}
+	}
+}
+
+bool CompareByAscending(int a, int b)
+{
+	return a > b;
+}
+
+bool CompareByDescending(int a, int b)
+{
+	return a < b;
 }
--- C/Parts/Part11.h
+++ C/Parts/Part11.h
@@ -9,4 +9,13 @@ void FuncB();
 /*
 함수포인터반환형 (*함수이름(함수의매개변수))(함수포인터의매개변수);
 */
-void (*FuncC(int funcType))();
+//void (*FuncC(int funcType))();
+typedef void (*FuncPtr_void_void)();
+FuncPtr_void_void FuncC(int funcType);	
+
+/*==============================================*/
+
+void FunctionPointerExample();
+void SortArray(int* arr, int len, bool(*compareFunc)(int, int));
+bool CompareByAscending(int a, int b);
+bool CompareByDescending(int a, int b);
